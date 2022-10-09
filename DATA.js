2e122471649608fd0eb023ed40e2499de0d4bf6c
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-09 15:57:54 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "ee57fb38-f6d4-4d6d-9bce-2cd4978a224f",
+    "home_feed": [
+        {
+            "id": "3f297bf2-48ef-427a-b9f2-0b6ba2440782",
+            "artist_name": "guy",
+            "home_carousel": [
+                {
+                    "id": "02a99d20-87c9-493d-8cb1-2c2738b6a7bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                },
+                {
+                    "id": "938e5233-3e42-4d39-9361-dd281e63cc75",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                },
+                {
+                    "id": "5af7faed-e9bc-487e-a922-f72e400311a1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png"
+                },
+                {
+                    "id": "d1217488-3bea-4fec-88be-e3a05c1c42a3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                },
+                {
+                    "id": "e819b08d-0e2e-4b0a-9d1d-d017fd2117e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/100.png"
+                }
+            ],
+            "art_caption": "institution",
+            "art_description": "Success force picture stuff however significant action attention. Often want protect data security loss. Table suggest rather quality during three response good."
+        },
+        {
+            "id": "65da54a9-8640-41dd-b549-aad7b1fa6f82",
+            "artist_name": "finally",
+            "home_carousel": [
+                {
+                    "id": "7d0e80b7-2824-4c35-8bbc-f8819760d946",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/202.png"
+                },
+                {
+                    "id": "8fa2c8c7-7f15-44ec-a2e6-e1cf7e1e3532",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                },
+                {
+                    "id": "b06193ff-cf7e-439d-a584-a0885c7a4acf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png"
+                },
+                {
+                    "id": "166988f3-a87d-4b47-9c50-59140e789c67",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+                },
+                {
+                    "id": "ff790dd5-0a01-4c1a-9d22-d612eba9637e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/351.png"
+                }
+            ],
+            "art_caption": "left",
+            "art_description": "Question next her of sound sort most wait. Economic TV the fill quickly majority force world."
+        },
+        {
+            "id": "aa66511d-801f-4660-8443-ff7643f33866",
+            "artist_name": "head",
+            "home_carousel": [
+                {
+                    "id": "4f94653f-4b7c-437b-9f6a-cb401fcc72ec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+                },
+                {
+                    "id": "c476cfdb-31ae-469b-84e4-9ee5ac47ce96",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png"
+                },
+                {
+                    "id": "b6dc222a-fbb1-430f-8a9e-160e7b2f2c40",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "00cda769-ffe6-4061-92d8-818d449d201c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                },
+                {
+                    "id": "0de4bb53-6613-4a6d-92a3-224383152b12",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                }
+            ],
+            "art_caption": "if",
+            "art_description": "Nature inside employee audience. Close strategy guy example natural artist. Tv add another industry too."
+        },
+        {
+            "id": "fa7f7f5f-91ea-47cc-af42-f00d19500a5a",
+            "artist_name": "range",
+            "home_carousel": [
+                {
+                    "id": "32551d2a-675d-4556-a525-69083e8b44fd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/212.png"
+                },
+                {
+                    "id": "1273fa1d-b103-431d-9052-662f28a14ff0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png"
+                },
+                {
+                    "id": "547642e8-4216-421f-827a-17fd7db9ce46",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "c055d605-7b37-42ea-812d-c0f6cfc4e774",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/242.png"
+                },
+                {
+                    "id": "fc4f9c4b-8b06-4f8b-ad52-14481e222275",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/338.png"
+                }
+            ],
+            "art_caption": "left",
+            "art_description": "Leg economy southern manager statement fund. Most able learn mouth. Prove listen relationship meeting."
+        },
+        {
+            "id": "0ec95eac-b265-49ca-970b-50dff14e2725",
+            "artist_name": "might",
+            "home_carousel": [
+                {
+                    "id": "65dd5d2b-92b0-486a-ad5a-7e6441030008",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/324.png"
+                },
+                {
+                    "id": "284433fb-722e-417e-8dc8-4fdcaae842bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png"
+                },
+                {
+                    "id": "5e8aa678-f8f6-47ea-a9f1-747fdd42ca4e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                },
+                {
+                    "id": "35f13542-cfa0-4db6-8656-6a57e900f9f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png"
+                },
+                {
+                    "id": "23582972-8db0-4910-b25f-928d10960830",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                }
+            ],
+            "art_caption": "citizen",
+            "art_description": "Talk them travel television prove. Own sit area image style about. Assume reduce responsibility manage."
+        }
+    ]
+}
